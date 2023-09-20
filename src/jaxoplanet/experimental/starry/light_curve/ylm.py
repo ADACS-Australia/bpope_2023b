02b3df2ec92a6936dd71eee06792407fcaa6d2b8
JHu <jitinghu@swin.edu.au>
2023-09-21 00:34:58 +1000

    re-design light_curve function for Ylm

--- src/jaxoplanet/experimental/starry/light_curve/ylm.py
+++ src/jaxoplanet/experimental/starry/light_curve/ylm.py
@@ -1,5 +1,7 @@
+from functools import partial
+
+import jax
 import jax.numpy as jnp
-import numpy as np
 
 from jaxoplanet.experimental.starry.basis import A1, A
 from jaxoplanet.experimental.starry.light_curve.utils import right_project
@@ -8,110 +10,42 @@ from jaxoplanet.experimental.starry.solution import rT_solution_vector, solution
 from jaxoplanet.types import Array
 
 
+@partial(jax.jit, static_argnames=("l_max", "order"))
 def light_curve(
     l_max: int,
-    theta: Array,
     xo: Array,
     yo: Array,
     zo: Array,
-    ro: float,
+    ro: Array,
     inc: float,
     obl: float,
+    theta: Array,
     y: Array,
+    order=10,
 ) -> Array:
-    design_matrix = X(l_max, theta, xo, yo, zo, ro, inc, obl)
-    # y = _get_y(l_max)
-    return design_matrix @ y
+    b = jnp.sqrt(xo**2 + yo**2)
+    theta_z = jnp.arctan2(xo, yo)
 
-
-def X(
-    l_max: int,
-    theta: Array,
-    xo: Array,
-    yo: Array,
-    zo: Array,
-    ro: float,
-    inc: float,
-    obl: float,
-) -> Array:
-    # Determine shapes
-    cols = (l_max + 1) ** 2
-    rows = theta.shape[0]
-    X = np.zeros((rows, cols))
-
-    # Compute the occultation mask
-    b = np.sqrt(xo**2 + yo**2)
+    # occultation mask
     cond_rot = (b >= (1.0 + ro)) | (zo <= 0.0) | (ro == 0.0)
-    cond_occ = ~cond_rot
-    i_rot = np.where(cond_rot)[0]
-    i_occ = np.where(cond_occ)[0]
+    cond_rot_ = cond_rot[:, jnp.newaxis]
 
-    # Rotational operator
-    if len(i_rot) > 0:
-        print("rotational operator")
-        rTA1 = rT_solution_vector(l_max) @ A1(l_max)
-        # Check for dimension
-        rTA1 = jnp.broadcast_to(rTA1, (rows, cols))
-        X[i_rot] = right_project(rTA1, inc, obl, theta[i_rot])
+    sT = solution_vector(l_max, order=order)(b, ro)
 
-    # Occulation operator
-    if len(i_occ) > 0:
-        print("occultation operator")
-        sT_func = solution_vector(l_max)
-        sT = sT_func(b[i_occ], ro)
-        sTA = sT @ A(l_max)
-        theta_z = np.arctan2(xo[i_occ], yo[i_occ])
-        sTAR = dotR(sTA, theta_z)
-        X[i_occ] = right_project(sTAR, inc, obl, theta[i_occ])
+    # applying occultation mask
+    sT_ = jnp.where(cond_rot_, sT, 0.0)
+    sTA = sT_ @ A(l_max)
 
-    return X
+    sTAR = dotR(l_max, [0, 0, 1])(sTA, theta_z)
 
+    # rotational phase
+    rTA1 = rT_solution_vector(l_max) @ A1(l_max)
+    rTA1_ = jnp.broadcast_to(rTA1, sTAR.shape)
 
-def _get_y(l_max: int) -> Array:
-    rows = (l_max + 1) ** 2
-    y = np.zeros((rows, 1))
-    y[0, 0] = 1
-    return y
+    # combine solution vectors
+    sTAR_ = jnp.where(~cond_rot_, sTAR, rTA1_)
 
+    # get the design matrix
+    X = right_project(l_max, sTAR_, inc, obl, theta)
 
-'''
-def flux(self, theta, xo, yo, zo, ro, inc, obl, y, u, f):
-        """Compute the light curve."""
-        return tt.dot(self.X(theta, xo, yo, zo, ro, inc, obl, u, f), y)
-
-def X(self, theta, xo, yo, zo, ro, inc, obl, u, f):
-        """Compute the light curve design matrix."""
-        # Determine shapes
-        rows = theta.shape[0]
-        cols = self.rTA1.shape[1]
-        X = tt.zeros((rows, cols))
-
-        # Compute the occultation mask
-        b = tt.sqrt(xo ** 2 + yo ** 2)
-        b_rot = tt.ge(b, 1.0 + ro) | tt.le(zo, 0.0) | tt.eq(ro, 0.0)
-        b_occ = tt.invert(b_rot)
-        i_rot = tt.arange(b.size)[b_rot]
-        i_occ = tt.arange(b.size)[b_occ]
-
-
-
-        # Rotation operator
-
-        rTA1 = self.rTA1
-        rTA1 = tt.tile(rTA1, (theta[i_rot].shape[0], 1))
-        X = tt.set_subtensor(
-            X[i_rot], self.right_project(rTA1, inc, obl, theta[i_rot])
-        )
-
-        # Occultation + rotation operator
-        sT = self.sT(b[i_occ], ro)
-        sTA = ts.dot(sT, self.A)
-        theta_z = tt.arctan2(xo[i_occ], yo[i_occ])
-        sTAR = self.tensordotRz(sTA, theta_z)
-
-        X = tt.set_subtensor(
-            X[i_occ], self.right_project(sTAR, inc, obl, theta[i_occ])
-        )
-
-        return X
-'''
+    return X @ y
--- tests/experimental/starry/light_curve/ylm_test.py
+++ tests/experimental/starry/light_curve/ylm_test.py
@@ -1,42 +1,10 @@
 import warnings
 
+import jax.numpy as jnp
 import numpy as np
 import pytest
-from jaxoplanet.experimental.starry.light_curve.ylm import X, light_curve
+from jaxoplanet.experimental.starry.light_curve.ylm import light_curve
 from jaxoplanet.test_utils import assert_allclose
-
-
-@pytest.mark.parametrize("l_max", [5, 4, 3, 2, 1, 0])
-def test_compare_starry_X(l_max):
-    starry = pytest.importorskip("starry")
-    theano = pytest.importorskip("theano")
-    theano.config.gcc__cxxflags += " -fexceptions"
-
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-
-        m = starry.Map(l_max)
-
-        theta = np.linspace(0, 360, 10)
-        xo = np.zeros(10)
-        yo = np.zeros(10)
-        zo = np.ones(10)
-        ro = 0.0
-
-        inc = 90
-        obl = 0
-
-        theta_tt, xo_tt, yo_tt, zo_tt = m._math.vectorize(theta, xo, yo, zo)
-        theta_tt, xo_tt, yo_tt, zo_tt, ro_tt = m._math.cast(
-            theta_tt, xo_tt, yo_tt, zo_tt, ro
-        )
-        expect = m.ops.X(
-            theta_tt, xo_tt, yo_tt, zo_tt, ro_tt, inc, obl, m._u, m._f
-        ).eval() * (0.5 * np.sqrt(np.pi))
-
-        calc = X(l_max, theta, xo, yo, zo, ro, inc, obl)
-
-        assert_allclose(calc, expect)
 
 
 @pytest.mark.parametrize("l_max", [5, 4, 3, 2, 1, 0])
@@ -46,29 +14,24 @@ def test_compare_starry_light_curve(l_max):
     theano = pytest.importorskip("theano")
     theano.config.gcc__cxxflags += " -fexceptions"
 
-    theta = np.linspace(0, 360, 10)
-    xo = np.zeros(10)
-    yo = np.zeros(10)
-    zo = np.ones(10)
-    ro = 0.0
-
-    inc = 90
-    obl = 0
-
-    N = (l_max + 1) ** 2
-    y = np.random.uniform(-5, 5, N)
+    ro = 0.1
+    xo = jnp.linspace(0, ro + 2, 500)
+    yo = jnp.zeros(500)
+    zo = jnp.zeros(500)
+    inc = 0
+    obl = np.pi / 2
+    theta = jnp.linspace(0, np.pi, 500)
+    n_max = (l_max + 1) ** 2
+    y = np.random.uniform(0, 1, n_max)
     y[0] = 1.0
 
-    m = starry.Map(l_max)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        m = starry.Map(l_max)
+        expect = m.ops.flux(theta, xo, yo, zo, ro, inc, obl, y, m._u, m._f) * (
+            0.5 * np.sqrt(np.pi)
+        )
 
-    # theta_tt, xo_tt, yo_tt, zo_tt, y_tt = m._math.vectorize(theta, xo, yo, zo, y)
-    # theta_tt, xo_tt, yo_tt, zo_tt, ro_tt = m._math.cast(
-    #         theta_tt, xo_tt, yo_tt, zo_tt, ro
-    # )
-    expect = m.ops.flux(theta, xo, yo, zo, ro, inc, obl, y, m._u, m._f) * (
-        0.5 * np.sqrt(np.pi)
-    )
-    print("expect: ", expect)
-    calc = light_curve(l_max, theta, xo, yo, zo, ro, inc, obl, y)
+    calc = light_curve(l_max, xo, yo, zo, ro, inc, obl, theta, y)
 
     assert_allclose(calc, expect)
