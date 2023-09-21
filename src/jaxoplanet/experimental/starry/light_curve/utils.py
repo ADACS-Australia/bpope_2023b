c737aa95ddf71813927bfffe8c04709a7b09e7db
JHu <jitinghu@swin.edu.au>
2023-09-21 13:45:13 +1000

    add ylm light_curve api

--- src/jaxoplanet/experimental/starry/light_curve/utils.py
+++ src/jaxoplanet/experimental/starry/light_curve/utils.py
@@ -1,8 +1,10 @@
+from typing import NamedTuple, Optional
+
 import jax
 import jax.numpy as jnp
 
 from jaxoplanet.experimental.starry.rotation import R_full
-from jaxoplanet.types import Array
+from jaxoplanet.types import Array, Scalar
 
 
 def right_project(l_max: int, M: Array, inc: float, obl: float, theta: Array):
@@ -48,3 +50,26 @@ def right_project(l_max: int, M: Array, inc: float, obl: float, theta: Array):
     M_ = M_cor @ R_full(l_max, ux)(0.5 * jnp.pi)
 
     return M_
+
+
+class RotationPhase(NamedTuple):
+    period: Scalar
+    t_0: Scalar
+    theta_0: Scalar
+
+    @classmethod
+    def init(
+        cls,
+        period: Scalar,
+        t_0: Optional[Scalar] = None,
+        theta_0: Optional[Scalar] = None,
+    ) -> "RotationPhase":
+        if not t_0:
+            t_0 = 0.0
+        if not theta_0:
+            theta_0 = 0.0
+
+        return cls(period=period, t_0=t_0, theta_0=theta_0)
+
+    def phase_curve(self, t: Array) -> Array:
+        return 2 * jnp.pi * (t - self.t_0) / self.period + self.theta_0
