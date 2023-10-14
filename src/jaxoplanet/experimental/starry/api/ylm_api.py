e307ef15e55aac5f86846e41f6abf6f5574e1852
JHu <jitinghu@swin.edu.au>
2023-10-15 09:14:11 +1100

    refactor api structure

---
+++ src/jaxoplanet/experimental/starry/api/ylm_api.py
@@ -0,0 +1,58 @@
+from typing import NamedTuple, Optional
+
+import jax.numpy as jnp
+import numpy as np
+
+from jaxoplanet.experimental.starry.light_curve.utils import RotationPhase
+from jaxoplanet.experimental.starry.light_curve.ylm import light_curve
+from jaxoplanet.proto import LightCurveOrbit
+from jaxoplanet.types import Array, Scalar
+
+
+class YlmLightCurve(NamedTuple):
+    l_max: int
+    inc: Scalar
+    obl: Scalar
+    y: Array
+
+    @classmethod
+    def init(
+        cls,
+        l_max: int,
+        inc: Optional[Scalar] = None,
+        obl: Optional[Scalar] = None,
+        y: Optional[Array] = None,
+    ) -> "YlmLightCurve":
+        if inc is None:
+            inc = jnp.pi / 2
+        if obl is None:
+            obl = 0.0
+        if y is None:
+            n_max = (l_max + 1) ** 2
+            y = np.zeros(n_max)
+            y[0] = 1.0
+
+        # TODO: add input checks and raise errors
+        return cls(l_max=l_max, inc=inc, obl=obl, y=y)
+
+    def light_curve(
+        self, orbit: LightCurveOrbit, phase: RotationPhase, t: Array
+    ) -> Array:
+        r_star = orbit.central_radius
+        xo, yo, zo = orbit.relative_position(t)
+
+        # convert to match dimensions
+        xo_ = xo.flatten()
+        yo_ = yo.flatten()
+        zo_ = zo.flatten()
+
+        ro = orbit.radius / r_star
+
+        theta = phase.phase_curve(t)
+
+        # lc_func = partial(light_curve, self.l_max, self.inc, self.obl, self.y)
+        lc = light_curve(
+            self.l_max, self.inc, self.obl, self.y, xo_, yo_, zo_, ro, theta
+        )
+        # lc = lc_func(xo_, yo_, zo_, ro, theta)
+        return lc
--- src/jaxoplanet/light_curves.py
+++ src/jaxoplanet/light_curves.py
@@ -2,15 +2,10 @@ from functools import partial
 from typing import NamedTuple, Optional
 
 import jax.numpy as jnp
-import numpy as np
 
 from jaxoplanet.core.limb_dark import light_curve
-from jaxoplanet.experimental.starry.light_curve.utils import RotationPhase
-from jaxoplanet.experimental.starry.light_curve.ylm import (
-    light_curve as ylm_light_curve,
-)
 from jaxoplanet.proto import LightCurveOrbit
-from jaxoplanet.types import Array, Scalar
+from jaxoplanet.types import Array
 
 
 class LimbDarkLightCurve(NamedTuple):
@@ -68,51 +63,3 @@ class LimbDarkLightCurve(NamedTuple):
             b /= r_star[..., None]
             lc = jnp.vectorize(lc_func, signature="(k),()->(k)")(b, r)
         return jnp.where(z > 0, lc, 0)
-
-
-class YlmLightCurve(NamedTuple):
-    l_max: int
-    inc: Scalar
-    obl: Scalar
-    y: Array
-
-    @classmethod
-    def init(
-        cls,
-        l_max: int,
-        inc: Optional[Scalar] = None,
-        obl: Optional[Scalar] = None,
-        y: Optional[Array] = None,
-    ) -> "YlmLightCurve":
-        if inc is None:
-            inc = jnp.pi / 2
-        if obl is None:
-            obl = 0
-        if y is None:
-            n_max = (l_max + 1) ** 2
-            y = np.zeros(n_max)
-            y[0] = 1.0
-
-        # TODO: add input checks and raise errors
-        return cls(l_max=l_max, inc=inc, obl=obl, y=y)
-
-    def light_curve(
-        self, orbit: LightCurveOrbit, phase: RotationPhase, t: Array
-    ) -> Array:
-        r_star = orbit.central_radius
-        xo, yo, zo = orbit.relative_position(t)
-
-        # convert to match dimensions
-        xo_ = xo.flatten()
-        yo_ = yo.flatten()
-        zo_ = zo.flatten()
-
-        ro = orbit.radius / r_star
-
-        theta = phase.phase_curve(t)
-
-        lc_func = partial(ylm_light_curve, self.l_max, self.inc, self.obl, self.y)
-        print("y type: ", type(self.y))
-
-        lc = lc_func(xo_, yo_, zo_, ro, theta)
-        return lc
