af0769ffca4713b8b777fd348e7904907abdd855
JHu <jitinghu@swin.edu.au>
2024-02-27 13:08:07 +1100

    debug

--- noxfile.py
+++ noxfile.py
@@ -18,7 +18,12 @@ def test(session, x64):
 
 @nox.session(python=ALL_PYTHON_VS)
 def comparison(session):
-    session.install(".[test,test-math,comparison]", "numpy==1.21.1", "xarray==2023.8.0")
+    session.install(
+        ".[test,test-math,comparison]",
+        "numpy==1.21.1",
+        "xarray==2023.8.0",
+        "astropy>=5.3",
+    )
     session.run("python", "-c", "import numpy; print(numpy.__version__)")
     session.run("pip", "freeze")
     session.run("python", "-c", "import starry")
